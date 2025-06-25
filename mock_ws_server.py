d1141fee1198e9f8badb0f9a92e5c8bd8db9cc07
jporter-xbrainerai <jporter@xbrainer.ai>
2025-06-24 21:31:04 -0700

    Fixed main method of server.

--- mock_ws_server.py
+++ mock_ws_server.py
@@ -2,8 +2,9 @@ import asyncio
 import websockets
 import json
 import random
+from websockets.asyncio.server import serve
 
-async def send_fake_data(websocket, path):
+async def send_fake_data(websocket):
     while True:
         message = {
             "timestamp": asyncio.get_event_loop().time(),
@@ -12,7 +13,9 @@ async def send_fake_data(websocket, path):
         await websocket.send(json.dumps(message))
         await asyncio.sleep(0.05)  # simulate 20 Hz stream
 
-start_server = websockets.serve(send_fake_data, "localhost", 8001)
 
-asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+async def main():
+    async with serve(send_fake_data, "localhost", 8001) as server:
+        await server.serve_forever()
+
+asyncio.run(main())
